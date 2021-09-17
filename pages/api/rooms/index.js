080bb16c4159937f856fb2583073ab99e722d8ed
Benneth <amaben202phantom@gmail.com>
2021-09-17 21:48:53 +0100

    connecting to db

--- pages/api/rooms/index.js
+++ pages/api/rooms/index.js
@@ -1,7 +1,9 @@
 import nc from 'next-connect';
 import { allRooms } from '../../../controllers/roomControllers';
-
+import dbConnect from '../../../config/dbConnect';
 const handler = nc();
+
+dbConnect();
 
 handler.get(allRooms);
 
